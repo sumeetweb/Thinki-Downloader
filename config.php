1c4380c78c4e23fe9283a7f449a66f386c358616
Sumeet Naik <25149022+sumeetweb@users.noreply.github.com>
2021-06-15 15:42:19 +0530

    Update extension line.

--- config.php
+++ config.php
@@ -9,6 +9,6 @@ if($cookiedata == '' || $clientdate == '')
 if(!extension_loaded('curl'))
 	$msg .= '\nCurl not installed or enabled in php.ini';
 if(!extension_loaded('mbstring'))
-	$msg .= '\nMbstring extension not enabled. Remove ; from php.ini config in the line ;extension=php_mbstring.dll';
+	$msg .= '\nMbstring extension not enabled. Remove ; from php.ini config in the line ;extension=mbstring';
 if($msg != '')
     die($msg);
