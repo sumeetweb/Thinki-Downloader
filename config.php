bb3587d764e7195baf6c9b5cf1fbbed63f79d51b
Tomáš Hnyk <tomashnyk@gmail.com>
2022-09-18 14:30:26 +0200

    Move newlines
    Now it appears like:
    
        Cookie data and Client Date not set. Use the ReadMe file first before using this script.username@hostanem~/Thinki-Downloader$ 
    
    with this, it appears like
    
        Cookie data and Client Date not set. Use the ReadMe file first before using this script.
        username@hostanem~/Thinki-Downloader$

--- config.php
+++ config.php
@@ -5,12 +5,12 @@ $cookiedata = '';
 // -:
 $msg = '';
 if($cookiedata == '' || $clientdate == '')
-    $msg .= "Cookie data and Client Date not set. Use the ReadMe file first before using this script.";
+    $msg .= "Cookie data and Client Date not set. Use the ReadMe file first before using this script.\n";
 if(!extension_loaded('curl'))
-	$msg .= '\nCurl not installed or enabled in php.ini';
+	$msg .= 'Curl not installed or enabled in php.ini\n';
 if(!extension_loaded('mbstring'))
-	$msg .= '\nMbstring extension not enabled. Remove ; from php.ini config in the line ;extension=mbstring';
+	$msg .= 'Mbstring extension not enabled. Remove ; from php.ini config in the line ;extension=mbstring\n';
 if(!extension_loaded('openssl'))
-	$msg .= '\nOpenssl not enabled in php.ini';
+	$msg .= 'Openssl not enabled in php.ini\n';
 if($msg != '')
     die($msg);
