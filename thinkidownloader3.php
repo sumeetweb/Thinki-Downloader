7e29ea9c80315ec51877218b2dc00d5fa6a96532
sumeetweb <25149022+sumeetweb@users.noreply.github.com>
2023-07-25 00:41:39 +0530

    Update message at start

--- thinkidownloader3.php
+++ thinkidownloader3.php
@@ -6,7 +6,7 @@ $root_project_dir = '';
 $revision = "Revision 6.2 ~ 3rd March 2023";
 
 error_reporting(0); //Disabled for keeping console clean. Set to 1 if you got an error or problem while downloading :)
-echo "THINKIFIC DOWNLOADER".PHP_EOL.$revision.PHP_EOL."Author : SumeetWeb ~ https://github.com/sumeetweb".PHP_EOL."Consider buying me a coffee at : https://www.ko-fi.com/sumeet".PHP_EOL."Thinki-Downloader Online is coming soon!".PHP_EOL;
+echo "THINKIFIC DOWNLOADER".PHP_EOL.$revision.PHP_EOL."Author : SumeetWeb ~ https://github.com/sumeetweb".PHP_EOL."Consider buying me a coffee at : https://www.ko-fi.com/sumeet".PHP_EOL."Thinki-Parser Experimental is available for testing!".PHP_EOL;
 echo "----------------------------------------------------------".PHP_EOL;
 require("include/file.functions.php");
 require("include/downloader.functions.php");
