18ffdcfd88c92004ab7a9b025f3c219670e585d5
sumeetweb <25149022+sumeetweb@users.noreply.github.com>
2024-11-29 01:17:07 +0530

    Refactor .env file usage in script

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
 
+load_env(__DIR__ . '/.env');
+
 $clientdate = $_ENV['CLIENT_DATE'];
 $cookiedata = $_ENV['COOKIE_DATA'];
 $video_download_quality = $_ENV['VIDEO_DOWNLOAD_QUALITY'];
 $FFMPEG_PRESENTATION_MERGE_FLAG = false;
+$current_directory_path = getcwd();
 $msg = '';
 
 if($cookiedata == '' || $clientdate == '')
@@ -14,5 +17,36 @@ if(!extension_loaded('mbstring'))
 	$msg .= 'Mbstring extension not enabled. Remove ; from php.ini config in the line ;extension=mbstring\n';
 if(!extension_loaded('openssl'))
 	$msg .= 'Openssl not enabled in php.ini\n';
+if(!$current_directory_path)
+	$msg .= 'Unable to get current working directory. Check if PHP or current terminal has permission to access the directory\n';
+if(!is_writable($current_directory_path))
+	$msg .= 'Current directory is not writable. Check if PHP or current terminal has permission to write in the directory\n';
 if($msg != '')
     die($msg);
+
+
+function load_env($filePath){
+	if (!file_exists($filePath)) {
+		throw new Exception("The .env file does not exist.");
+	}
+
+	$lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+	foreach ($lines as $line) {
+		// Skip comments
+		if (strpos(trim($line), '#') === 0) {
+			continue;
+		}
+
+		// Split by '=' to separate key and value
+		[$name, $value] = array_map('trim', explode('=', $line, 2));
+
+		// Remove surrounding quotes from the value
+		$value = trim($value, "'\"");
+
+		// Set the environment variable
+		$_ENV[$name] = $value;
+		$_SERVER[$name] = $value;
+		putenv("$name=$value");
+	}
+}
--- thinkidownloader3.php
+++ thinkidownloader3.php
@@ -13,16 +13,41 @@ require("include/downloader.functions.php");
 require("include/wistia.downloader.php");
 
 // Run.
-// If --json, then read from json file.
-// Else, download from url.
-if(in_array("--json", $argv) && isset($argv[2])) {
-	$json = file_get_contents($argv[2]);
+// If --json or COURSE_DATA_FILE in env, then read from json file.
+// Else, download from url or use COURSE_URL from env.
+if( (in_array("--json", $argv) && isset($argv[2])) || !in_array(getenv("COURSE_DATA_FILE"), ["", null, false]) ) {
+
+	// --json is higher priority than env.
+	if (in_array("--json", $argv)) {
+		echo "Using Custom Metadata File for course data.".PHP_EOL;
+		if (!file_exists($argv[2])) {
+			die("File not found: ".$argv[2].PHP_EOL);
+		}
+		
+		$json = file_get_contents($argv[2]);
+	} else {
+		echo "Loading Custom Metadata File from .env for course data.".PHP_EOL;
+		$json = file_get_contents(getenv("COURSE_DATA_FILE"));
+	}
+
 	$data = json_decode($json, true);
 	$contentsdata = $data["contents"];
 	init_course($data);
 } else if(isset($argv[1])) {
-	$url = query($argv[1]);
-	$p = parse_url($argv[1]);
+	// Use course url from command line. It is higher priority than env.
+	$courseUrl = $argv[1];
+	handler($courseUrl);
+} else if(!in_array(getenv("COURSE_URL"), ["", null, false])) {
+	$courseUrl = getenv("COURSE_URL");
+	handler($courseUrl);
+} else {
+	echo "Usage for using course url: php thinkidownloader3.php <course_url>".PHP_EOL;
+	echo "Usage for selective download: php thinkidownloader3.php --json <course.json>".PHP_EOL;
+}
+
+function handler($courseUrl) {
+	$url = query($courseUrl);
+	$p = parse_url($courseUrl);
 	$path = $p;
 	$path = explode("/", $path["path"]); 
 	file_put_contents(end($path).".json",$url); //save coursename.json
@@ -33,8 +58,5 @@ if(in_array("--json", $argv) && isset($argv[2])) {
 	else
 		echo "Fetching Course Contents... Please Wait...".PHP_EOL;
 	init_course($data);
-} else {
-	echo "Usage for using course url: php thinkidownloader3.php <course_url>".PHP_EOL;
-	echo "Usage for selective download: php thinkidownloader3.php --json <course.json>".PHP_EOL;
 }
 ?>
